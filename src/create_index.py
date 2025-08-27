eb95c37c8f043d8a610a928afbae6963a19a3f2c
Hood Chatham <roberthoodchatham@gmail.com>
2025-08-27 11:47:53 +0200

    Add index page, add favicons, fix first load

--- src/create_index.py
+++ src/create_index.py
@@ -1,13 +1,63 @@
 from urllib.parse import urlparse
 from textwrap import dedent
 
+
+ROOT_INDEX_TEMPLATE = dedent(
+    """
+    <!DOCTYPE html>
+    <html>
+    <head>
+    <meta name="pypi:repository-version" content="1.0">
+    <title>Pyodide Simple Package Indices</title>
+    <link rel="icon" type="image/x-icon" href="/assets/favicon.ico">
+    <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon-16x16.png">
+    </head>
+    <body>
+    <h1>Pyodide simple package indices</h1>
+    <p>
+    This is a collection of Pyodide simple package indices.
+    The most recent one is here <a href={latest}>{latest}</a>.
+    You can pass the url to that page as an <code>--extra-index-url</code> for
+    pip or uv to install packages for Pyodide. Though it's unlikely you need to
+    do this directly.
+    </p>
+
+    <h2>All available versions</h2>
+    {all_versions}
+    </body>
+    </html>
+    """
+).strip()
+
+
+def make_root_index_page(version_json):
+    latest = version_json["tags"]["latest"]
+    # Skip alpha versions. Before 0.24.0 we didn't have a pyodide-lock.json so
+    # this won't work.
+    all_versions = (
+        key
+        for key in version_json["versions"]
+        if "alpha" not in key and "dev" not in key and key >= "0.24.0"
+    )
+    versions_html = []
+    for ver in all_versions:
+        versions_html.append(f"<div><a href={ver}>{ver}</a></div>")
+    return ROOT_INDEX_TEMPLATE.format(
+        latest=latest, all_versions="\n".join(versions_html)
+    )
+
+
 INDEX_TEMPLATE = dedent(
     """
     <!DOCTYPE html>
     <html>
     <head>
     <meta name="pypi:repository-version" content="1.0">
-    <title>Simple index</title>
+    <title>Pyodide {version} Simple Package Index</title>
+    <link rel="icon" type="image/x-icon" href="/assets/favicon.ico">
+    <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon-16x16.png">
     </head>
     <body>
     {packages_str}
@@ -22,7 +72,10 @@ FILE_TEMPLATE = dedent(
     <html>
     <head>
     <meta name="pypi:repository-version" content="1.0">
-    <title>Links for {pkgname}</title>
+    <title>Links for {pkgname} from Pyodide {version} Simple Package Index</title>
+    <link rel="icon" type="image/x-icon" href="/assets/favicon.ico">
+    <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon-16x16.png">
     </head>
     <body>
     <h1>Links for {pkgname}</h1>
@@ -31,6 +84,7 @@ FILE_TEMPLATE = dedent(
     </html>
     """
 ).strip()
+
 
 def create_package_index(
     version: str, packages, dist_url: str
@@ -46,7 +100,12 @@ def create_package_index(
     packages_str = "\n".join(f'<a href="{x}/">{x}</a>' for x in packages.keys())
 
     result = []
-    result.append((f"/{version}/index.html", INDEX_TEMPLATE.format(packages_str=packages_str)))
+    result.append(
+        (
+            f"/{version}/index.html",
+            INDEX_TEMPLATE.format(version=version, packages_str=packages_str),
+        )
+    )
 
     for pkgname, pkginfo in packages.items():
         filename = pkginfo["file_name"]
@@ -56,6 +115,8 @@ def create_package_index(
             shasum = pkginfo["sha256"]
             href = f"{dist_url}{filename}#sha256={shasum}"
         links_str = f'<a href="{href}">{pkgname}</a>\n'
-        files_html = FILE_TEMPLATE.format(pkgname=pkgname, links=links_str)
+        files_html = FILE_TEMPLATE.format(
+            version=version, pkgname=pkgname, links=links_str
+        )
         result.append((f"/{version}/{pkgname}/index.html", files_html))
     return result
--- src/worker.py
+++ src/worker.py
@@ -1,36 +1,50 @@
 from workers import Response, WorkerEntrypoint, fetch
 from urllib.parse import urlparse
-from create_index import create_package_index
+from create_index import create_package_index, make_root_index_page
 from js import Headers
 
 DIST_TEMPLATE = "https://cdn.jsdelivr.net/pyodide/v{}/full/"
-HEADERS = Headers.new([
-    ("access-control-allow-origin", "*"),
-    ("access-control-expose-headers", "*"),
-    ("content-type", "text/html"),
-])
+HEADERS = Headers.new(
+    [
+        ("access-control-allow-origin", "*"),
+        ("access-control-expose-headers", "*"),
+        ("content-type", "text/html"),
+    ]
+)
+
 
 class Default(WorkerEntrypoint):
     async def fetch(self, request):
         path = urlparse(request.url).path
-        if path == "/favicon":
-            return Response("")
+        if path.startswith("/assets"):
+            return await self.env._env.ASSETS.fetch(
+                "http://fakehost.invalid/" + path.removeprefix("/assets")
+            )
+        if not path.endswith("/index.html") and not path.endswith("/"):
+            path += "/"
         if path.endswith("/"):
             path += "index.html"
+        if path == "/index.html":
+            resp = await fetch("https://data.jsdelivr.com/v1/package/npm/pyodide")
+            resp.raise_for_status()
+            version_json = await resp.json()
+            html = make_root_index_page(version_json)
+            return Response(html, headers=HEADERS)
+
         version = path.split("/", maxsplit=2)[1]
 
         result = await self.env.index_cache.get(path)
         if result:
-            return Response(result)
+            return Response(result, headers=HEADERS)
 
         dist_url = DIST_TEMPLATE.format(version)
         lock_url = dist_url + "pyodide-lock.json"
-        print("Fetching url", lock_url)
         resp = await fetch(lock_url)
         resp.raise_for_status()
         lock = await resp.json()
         idx = create_package_index(version, lock["packages"], dist_url)
         for key, val in idx:
+            if key == path:
+                result = val
             await self.env.index_cache.put(key, val)
-        result = await self.env.index_cache.get(path)
         return Response(result, headers=HEADERS)
